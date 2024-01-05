e576eba1a187c72bdf132bfcc4ad6e3c86bede08
mmm606 <mmm606@scarletmail.rutgers.edu>
2024-01-04 22:04:01 -0500

    having issue still

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -22,8 +22,17 @@ end
 
 local function cmp_config()
     local cmp = require("cmp")
-    local cmp_action = require("lsp-zero")
+    local cmp_action = require("lsp-zero").cmp_action()
     cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
         mapping = cmp.mapping.preset.insert({
 
             ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
@@ -38,10 +47,19 @@ local function cmp_config()
             -- Navigate between snippet placeholder
             -- ['<Tab>'] = cmp_action.luasnip_jump_forward(),
             -- ['<S-Tab>'] = cmp_action.luasnip_jump_backward(),
+            -- cmp_action.luasnip.supertab(),
 
             -- Scroll up and down in the completion documentation
             ['<C-M-j>'] = cmp.mapping.scroll_docs(4),
             ['<C-M-k>'] = cmp.mapping.scroll_docs(-4),
+
+        sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'luasnip' },
+            }, {
+              { name = 'buffer' },
+            })
+
         })
     })
 end
